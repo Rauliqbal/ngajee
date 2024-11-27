4b3a94251ed964c85ca7ac7180c9a3865f4dd4bb
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2024-11-27 19:17:18 +0700

    refactor: ubah SSR menjadi CSR

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -4,47 +4,47 @@ const { data: surat } = await useFetch("https://equran.id/api/v2/surat");
 const userName = ref(getItem("username"));
 
 function getItem(item) {
-  if (process.client) {
-    return localStorage.getItem(item);
-  } else {
-    return undefined;
-  }
+   if (process.client) {
+      return localStorage.getItem(item);
+   } else {
+      return undefined;
+   }
 }
 </script>
 <template>
-  <div>
-    <Navbar />
-    <main class="pt-28 pb-24 md:pb-0 container">
-      <div
-        class="bg-gradient-to-br from-primary to-emerald-700 dark:bg-gradient-to-br dark:from-teal-800 dark:to-teal-900 p-5 md:p-8 flex flex-col px-6 py-4 rounded-2xl shadow-md"
-      >
-        <p class="text-xl md:text-2xl text-white">Assalamulaikum</p>
-        <p class="text-xl md:text-3xl text-white font-semibold mt-1">
-          {{ userName }}
-        </p>
+   <div>
+      <Navbar />
+      <main class="container pb-24 pt-28 md:pb-0">
+         <div
+            class="flex flex-col p-5 px-6 py-4 shadow-md bg-gradient-to-br from-primary to-emerald-700 dark:bg-gradient-to-br dark:from-teal-800 dark:to-teal-900 md:p-8 rounded-2xl"
+         >
+            <p class="text-xl text-white md:text-2xl">Assalamulaikum</p>
+            <p class="mt-1 text-xl font-semibold text-white md:text-3xl">
+               {{ userName }}
+            </p>
 
-        <!-- <UInput
+            <!-- <UInput
           color="primary"
           variant="outline"
           placeholder="Search..."
           v-model="filter"
         /> -->
-      </div>
+         </div>
 
-      {{ list }}
+         {{ list }}
 
-      <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 mt-10">
-        <CardSurat
-          v-for="i in surat.data"
-          :key="i.nomor"
-          :nomor="i.nomor"
-          :nama="i.nama"
-          :nama-latin="i.namaLatin"
-          :jumlah-ayat="i.jumlahAyat"
-          :tempat-turun="i.tempatTurun"
-        />
-      </div>
-    </main>
-    <Footer />
-  </div>
+         <div class="grid gap-4 mt-10 md:grid-cols-2 lg:grid-cols-3 md:gap-6">
+            <CardSurat
+               v-for="i in surat?.data"
+               :key="i.nomor"
+               :nomor="i.nomor"
+               :nama="i.nama"
+               :nama-latin="i.namaLatin"
+               :jumlah-ayat="i.jumlahAyat"
+               :tempat-turun="i.tempatTurun"
+            />
+         </div>
+      </main>
+      <Footer />
+   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,56 @@
 <script setup>
+import { useRuntimeConfig } from "#imports";
+
 const isOpen = ref(true);
 const name = ref("");
+const { app } = useRuntimeConfig();
 
 definePageMeta({
-  middleware: "auth",
+   middleware: "auth",
 });
 
 function setName() {
-  if (process.client) {
-    localStorage.setItem("username", name.value);
-    navigateTo("/dashboard");
-  } else {
-    return undefined;
-  }
+   if (app.isClient) {
+      localStorage.setItem("username", name.value);
+      navigateTo("/dashboard");
+   } else {
+      return undefined;
+   }
 }
 </script>
 <template>
-  <main
-    class="h-screen flex items-center justify-center bg-primary dark:bg-dark-primary"
-  >
-    <h1
-      class="text-5xl md:text-6xl font-bold text-white relative after:content[''] after:relative after:bottom-0 after:left-1 after:block after:bg-white after:w-8 after:rounded-full after:h-[6px]"
-    >
-      Ngajee.
-    </h1>
+   <main
+      class="flex items-center justify-center h-screen bg-primary dark:bg-dark-primary"
+   >
+      <h1
+         class="text-5xl md:text-6xl font-bold text-white relative after:content[''] after:relative after:bottom-0 after:left-1 after:block after:bg-white after:w-8 after:rounded-full after:h-[6px]"
+      >
+         Ngajee.
+      </h1>
 
-    <UModal v-model="isOpen" prevent-close>
-      <div class="p-4 md:px-5 md:py-8">
-        <img class="w-60 mx-auto mb-10" src="~/assets/images/quran.png" />
-        <h3 class="text-xl md:text-2xl font-semibold text-center">
-          Selamat datang di Ngajee.
-        </h3>
-        <p class="dark:text-gray-300 text-gray-500 text-center mt-4">
-          Silahkan masukkan namamu terlebih dahulu
-        </p>
+      <UModal v-model="isOpen" prevent-close>
+         <div class="p-4 md:px-5 md:py-8">
+            <img class="mx-auto mb-10 w-60" src="~/assets/images/quran.png" />
+            <h3 class="text-xl font-semibold text-center md:text-2xl">
+               Selamat datang di Ngajee.
+            </h3>
+            <p class="mt-4 text-center text-gray-500 dark:text-gray-300">
+               Silahkan masukkan namamu terlebih dahulu
+            </p>
 
-        <form @submit.prevent="setName" class="flex flex-col mt-8">
-          <label>
-            <input
-              class="w-full py-4 pl-5 pr-4 rounded-full outline-none focus:ring-primary focus:ring-2 border"
-              type="text"
-              placeholder="Masukkan Namamu"
-              v-model="name"
-            />
-          </label>
+            <form @submit.prevent="setName" class="flex flex-col mt-8">
+               <label>
+                  <input
+                     class="w-full py-4 pl-5 pr-4 border rounded-full outline-none focus:ring-primary focus:ring-2"
+                     type="text"
+                     placeholder="Masukkan Namamu"
+                     v-model="name"
+                  />
+               </label>
 
-          <button type="submit" class="btn-primary mt-8">Submit</button>
-        </form>
-      </div>
-    </UModal>
-  </main>
+               <button type="submit" class="mt-8 btn-primary">Submit</button>
+            </form>
+         </div>
+      </UModal>
+   </main>
 </template>
